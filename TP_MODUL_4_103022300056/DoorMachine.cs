bd895cdf8206cd1604f8bff533f2df8f57cb32cd
SleepyBear-UI <daffamantap29@gmail.com>
2025-03-10 20:08:05 +0700

    menambahkan implementasi dengan state-based constructio

---
+++ TP_MODUL_4_103022300056/DoorMachine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DoorStateMachine
+{
+    // Enum untuk merepresentasikan state
+    public enum DoorState
+    {
+        Terkunci,
+        Terbuka
+    }
+
+    // Class DoorMachine
+    public class DoorMachine
+    {
+        private DoorState currentState;
+
+        // Constructor, state awal adalah Terkunci
+        public DoorMachine()
+        {
+            currentState = DoorState.Terkunci;
+            DisplayState();
+        }
+
+        // Method untuk menampilkan state saat ini
+        private void DisplayState()
+        {
+            switch (currentState)
+            {
+                case DoorState.Terkunci:
+                    Console.WriteLine("Pintu terkunci");
+                    break;
+                case DoorState.Terbuka:
+                    Console.WriteLine("Pintu tidak terkunci");
+                    break;
+            }
+        }
+
+        // Method untuk mengubah state
+        public void SetState(DoorState newState)
+        {
+            currentState = newState;
+            DisplayState();
+        }
+    }
+
+}
--- TP_MODUL_4_103022300056/Program.cs
+++ TP_MODUL_4_103022300056/Program.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using DoorStateMachine;
 
 public class Program
 {
     public static void Main()
     {
-        KodePos kodePos = new KodePos();
+        //KodePos kodePos = new KodePos();
 
-        Console.WriteLine("Kode pos Maleer: " + kodePos.GetKodePos("Maleer"));
-        Console.WriteLine("Kode pos Sekejati: " + kodePos.GetKodePos("Sekejati"));
-        Console.WriteLine("Kode pos Xyz: " + kodePos.GetKodePos("Xyz")); // yang tidak ada
+        //Console.WriteLine("Kode pos Maleer: " + kodePos.GetKodePos("Maleer"));
+        //Console.WriteLine("Kode pos Sekejati: " + kodePos.GetKodePos("Sekejati"));
+        //Console.WriteLine("Kode pos Xyz: " + kodePos.GetKodePos("Xyz")); // yang tidak ada
+
+        DoorMachine door = new DoorMachine();
+
+        // Simulasi perubahan state
+        door.SetState(DoorState.Terbuka);
+        door.SetState(DoorState.Terkunci);
     }
 }
